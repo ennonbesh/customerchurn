e8f83916fa23e258c841e93ba1db49559392cb3e
ennonbesh <akusepakkang@gmail.com>
2025-02-06 12:23:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 
 #import streamlit as st
-#import shap
+import shap
 import pandas as pd
-#import numpy as np
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
